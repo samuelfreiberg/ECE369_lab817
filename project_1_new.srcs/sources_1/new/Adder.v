c5d51977df5611d103284353834d259f671e9942
Sammy Freiberg <sammyfreiberg@gmail.com>
2017-10-30 15:59:33 -0700

    fix

---
+++ project_1_new.srcs/sources_1/new/Adder.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/26/2017 01:59:05 PM
+// Design Name: 
+// Module Name: Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Adder(pc_plus4, shiftleft, out);
+
+    input [31:0] pc_plus4, shiftleft;
+    output reg [31:0] out;
+    
+    initial begin
+        out <= 0;
+    end    
+    
+    always @(pc_plus4 or shiftleft) begin
+        out <= pc_plus4 + shiftleft;
+    end    
+
+endmodule
